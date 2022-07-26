6f76f57bd13af3fd895094a3ea90bd6fdfa6e404
Élise Rubio <elise.rubio@4sh.fr>
2022-07-26 15:17:30 +0200

    :tada: Init api execution report module This module will be used to share all the API objects

---
+++ modules/api-execution-report/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    id("maven-publish")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            from(components["java"])
+        }
+    }
+}
