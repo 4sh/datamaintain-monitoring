53400e0667a69229b29d9a808e4aff78f1b2d04f
Élise Rubio <elise.rubio@4sh.fr>
2022-07-26 17:19:54 +0200

    :arrow_up: Use latest kotlin-logging version

--- modules/app-server/rest/build.gradle.kts
+++ modules/app-server/rest/build.gradle.kts
@@ -3,9 +3,9 @@ plugins {
 }
 
 dependencies {
-    implementation("io.github.microutils:kotlin-logging:1.8.0.1")
-
     implementation(project(":modules:api-execution-report"))
+
+    implementation(logging())
     implementation(http4k("core"))
     implementation(http4k("contract"))
     implementation(http4k("format-jackson"))
