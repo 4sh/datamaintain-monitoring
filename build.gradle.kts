126a53037bcd1ae90ad5b769eab50f5ec886b1c5
Élise Rubio <elise.rubio@4sh.fr>
2022-07-26 15:16:12 +0200

    :alembic: Add server that listens on port 8080

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.21"
+    id("com.palantir.git-version") version "0.12.3"
     application
 }
 
-group = "io.github.4sh.datamaintain"
+group = "io.github.4sh.datamaintain-monitoring"
 version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
 
 dependencies {
     testImplementation(kotlin("test"))
@@ -24,6 +21,22 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-application {
-    mainClass.set("MainKt")
+allprojects {
+    apply<com.palantir.gradle.gitversion.GitVersionPlugin>()
+
+    val versionDetails: groovy.lang.Closure<com.palantir.gradle.gitversion.VersionDetails> by extra
+    val lastTag = versionDetails().lastTag
+
+    val computedVersion: String = if (lastTag != "") {
+        lastTag
+    } else {
+        "SNAPSHOT"
+    }
+
+    group = "io.github.4sh.datamaintain-monitoring"
+    version = computedVersion
+
+    repositories {
+        mavenCentral()
+    }
 }
