92576b3e05725f1ab4c8c7a66167d17aafacdbf1
Élise Rubio <elise.rubio@4sh.fr>
2022-07-26 17:18:00 +0200

    :arrow_up: Use latest http4k

--- modules/app-server/app/build.gradle.kts
+++ modules/app-server/app/build.gradle.kts
@@ -6,5 +6,5 @@ dependencies {
     implementation(project(":modules:app-server:rest"))
 
     implementation("io.github.microutils:kotlin-logging:1.8.0.1")
-    implementation("org.http4k:http4k-server-netty:4.3.4.1")
+    implementation(http4k("server-netty"))
 }
