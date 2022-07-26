40f9e526a0e9862172459bd1e569d12e0f9f64dc
Élise Rubio <elise.rubio@4sh.fr>
2022-07-26 17:32:36 +0200

    :arrow_up: Use latest kotlin & java

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version "1.7.0"
     id("com.palantir.git-version") version "0.12.3"
     application
 }
@@ -18,7 +18,7 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "17"
 }
 
 allprojects {
