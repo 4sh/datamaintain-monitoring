6516c96372c0b49170f507733ccf24a66170faf9
Élise Rubio <elise.rubio@4sh.fr>
2022-07-26 15:16:12 +0200

    :alembic: Add server that listens on port 8080

--- modules/app-server/app/build.gradle.kts
+++ modules/app-server/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("org.jetbrains.kotlin.jvm")
 }
 
-repositories {
-    mavenCentral()
+dependencies {
+    implementation("org.http4k:http4k-core:4.3.4.1")
+    implementation("org.http4k:http4k-server-netty:4.3.4.1")
 }
