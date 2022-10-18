6e53430abbbf029bd91838b8cbec262791001088
driccio <damien.riccio@4sh.fr>
2022-10-18 17:40:52 +0200

    refacto: Remove useless method on ProjectService

--- modules/ui/src/lib/services/ProjectService.ts
+++ modules/ui/src/lib/services/ProjectService.ts
@@ -2,43 +2,7 @@ import type {ProjectHierarchy} from "../domain/ProjectHierarchy";
 import {ProjectMock} from "../mocks/ProjectMock";
 import type {Project} from "../domain/Project";
 
-export type ProjectSearchRequest = { q: string };
-
 export class ProjectService {
-    public static search(request: ProjectSearchRequest, skip?: number, limit?: number): Project[]  {
-        let projects = ProjectMock.projects;
-
-        if (request.q) {
-            projects = projects
-                .filter(project =>
-                    project.name.startsWith(request.q)
-                );
-        }
-
-        if(skip) {
-            projects = projects.slice(skip, projects.length)
-        }
-
-        if(limit) {
-            projects = projects.slice(0, limit)
-        }
-
-        return projects;
-    }
-
-    public static count(request: ProjectSearchRequest): number  {
-        let projects = ProjectMock.projects;
-
-        if (request.q) {
-            projects = projects
-                .filter(project =>
-                    project.name.startsWith(request.q)
-                );
-        }
-
-        return projects.length;
-    }
-
     public static byId(id: string): Promise<Project> {
         return new Promise<Project>((resolve, reject) => {
             const project = ProjectMock.projects.find(project => project.id === id);
