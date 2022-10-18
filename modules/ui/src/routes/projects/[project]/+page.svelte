99e1fdfc72b483141ace26d4fce0ef453a264b6c
driccio <damien.riccio@4sh.fr>
2022-10-18 16:40:13 +0200

    feat: Get project from service

--- modules/ui/src/routes/projects/[project]/+page.svelte
+++ modules/ui/src/routes/projects/[project]/+page.svelte
@@ -1,1 +1,12 @@
-Project [name]
+<script lang="ts">
+   import {ProjectService} from "../../../lib/services/ProjectService";
+   import {page} from "$app/stores";
+
+   let project
+
+   $: if($page.params?.project) {
+       project = ProjectService.byId($page.params.project);
+   }
+</script>
+
+Project {project.name}
