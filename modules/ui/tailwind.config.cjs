9e3d8907ef1adeed9e5ca520662171ee81ce218d
driccio <damien.riccio@4sh.fr>
2022-10-27 08:15:27 +0200

    chore: add Tailwind css lib

---
+++ modules/ui/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './src/app.html',
+      './src/routes/**/*.svelte',
+      './src/lib/components/**/*.svelte'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
