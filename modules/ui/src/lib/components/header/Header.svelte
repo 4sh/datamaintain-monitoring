2a6ecaa6166400db1532d834abe672a5fc818c0d
wandabardey <wanda.bardey@4sh.fr>
2023-03-21 15:32:52 +0100

    start skin header

--- modules/ui/src/lib/components/header/Header.svelte
+++ modules/ui/src/lib/components/header/Header.svelte
@@ -1,25 +1,55 @@
 <script lang="ts">
-import CurrentUserSubHeader from "./CurrentUserSubHeader.svelte";
 import ProjectHierarchy from "../menu/Hierarchy.svelte";
 </script>
 
 <div class="header">
 	<div class="header-logo">
 		<div class="header-logo-img">
-
+			<img src="src/assets/images/svg/datamaintain-logo-picto.svg" alt="Logo Datamaintain Picto">
 		</div>
 		<div class="header-logo-name">
-
+			<img src="src/assets/images/svg/datamaintain-logo-letters.svg" alt="Logo Datamaintain Letters">
 		</div>
 	</div>
-	<CurrentUserSubHeader></CurrentUserSubHeader>
-	<ProjectHierarchy></ProjectHierarchy>
+	<div class="header-hierarchy">
+		<ProjectHierarchy></ProjectHierarchy>
+	</div>
 </div>
 
 <style lang="scss">
 	@import "src/app";
 
 	.header {
+		height: 100%;
 
+		&-logo {
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			padding: 30px;
+
+			&-img {
+				background-color: $bg-logo;
+				height: 56px;
+				width: 56px;
+				border-radius: 8px;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+			}
+
+			&-name {
+				max-width: calc(100% - 80px);
+				margin-left: 24px;
+
+				img {
+					width: 100%;
+				}
+			}
+		}
+
+		&-hierarchy {
+			padding: 15px 30px;
+		}
 	}
 </style>
