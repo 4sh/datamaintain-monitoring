90203e4be77f88bc0e4d49206176b9c8aa9e44e1
wandabardey <wanda.bardey@4sh.fr>
2023-03-21 18:01:06 +0100

    skin search and icon component

---
+++ modules/ui/src/lib/components/atoms/A_icon.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let type: string;
+    export let size: string;
+</script>
+
+<span class="material-symbols-outlined _{size}">{type}</span>
+
+<style lang="scss">
+  .material-symbols-outlined {
+    font-family: 'Material Icons Two Tone';
+    font-variation-settings: 'FILL' 0,
+    'wght' 400,
+    'GRAD' 0,
+    'opsz' 48;
+
+    /* SIZE */
+
+    &._small {
+      font-size: 12px;
+    }
+  }
+</style>
---
+++ modules/ui/src/lib/components/molecules/M_inputSearch.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import A_icon from "$lib/components/atoms/A_icon.svelte";
+</script>
+
+<div class="inputSearch">
+    <div class="inputSearch-input">
+        <input type="text" on:change placeholder="Rechercher">
+    </div>
+    <div class="inputSearch-icon">
+        <A_icon type="search" size="small"/>
+    </div>
+</div>
+
+<style lang="scss">
+  @import "src/app";
+
+  .inputSearch {
+    background-color: $bg-color;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 13px;
+    border-radius: 4px;
+
+    &-input {
+      max-width: calc(100% - 20px);
+
+      input {
+        border: none;
+        background: transparent;
+        outline: none;
+        font-weight: 400;
+        color: $secondary-color;
+
+        &::placeholder {
+          color: $secondary-color;
+          font-weight: lighter;
+          font-style: italic;
+        }
+      }
+    }
+
+    &-icon {
+    }
+  }
+</style>
