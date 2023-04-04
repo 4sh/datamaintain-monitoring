9c6a1be62b2ccbcbf4dd5fd280d562bb2deaefb8
asolovieff <alexandre.solovieff@4sh.fr>
2023-04-04 16:06:52 +0200

    chore: Add jooq configuration to build.gradle.kts

--- modules/app-server/domain/build.gradle.kts
+++ modules/app-server/domain/build.gradle.kts
@@ -1,7 +1,90 @@
+import nu.studer.gradle.jooq.JooqEdition
+import org.jooq.meta.jaxb.Logging
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    id("org.jetbrains.kotlin.jvm")
+    id("nu.studer.jooq") version "8.1"
+    kotlin("jvm")
 }
 
 repositories {
     mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    jooqGenerator("org.postgresql:postgresql:42.5.1")
+    implementation("jakarta.validation:jakarta.validation-api:3.0.2")
+}
+
+jooq {
+    version.set("3.18.2")  // default (can be omitted)
+    edition.set(JooqEdition.OSS)  // default (can be omitted)
+
+    configurations {
+        create("main") {  // name of the jOOQ configuration
+            generateSchemaSourceOnCompilation.set(true)  // default (can be omitted)
+
+            jooqConfiguration.apply {
+                logging = Logging.WARN
+                jdbc.apply {
+                    driver = "org.postgresql.Driver"
+                    url = "jdbc:postgresql://localhost:5432/datamaintain"
+                    user = "datamaintain"
+                    password = "datamaintain"
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.KotlinGenerator"
+                    database.apply {
+                        name = "org.jooq.meta.postgres.PostgresDatabase"
+                        inputSchema = "public"
+                    }
+                    generate.apply {
+                        isDeprecated = false
+                        isRecords = true
+                        isImmutablePojos = true
+                        isFluentSetters = true
+                        isImplicitJoinPathsAsKotlinProperties = true
+                        isKotlinSetterJvmNameAnnotationsOnIsPrefix = true
+                        isPojosAsKotlinDataClasses = true
+                        isValidationAnnotations = true
+                    }
+                    target.apply {
+                        packageName = "jooq.generated.domain"
+                        directory = "${project.rootDir}/modules/app-server/domain/src/jooq"
+                    }
+                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+                }
+            }
+        }
+    }
+}
+
+buildscript {
+    configurations["classpath"].resolutionStrategy.eachDependency {
+        if (requested.group == "org.jooq") {
+            useVersion("3.18.2")
+        }
+    }
+}
+
+tasks.named<nu.studer.gradle.jooq.JooqGenerate>("generateJooq") {
+    (launcher::set)(javaToolchains.launcherFor {
+        languageVersion.set(JavaLanguageVersion.of(18))
+    })
+}
+
+tasks.named("clean") {
+    doLast {
+        file("src/jooq").deleteRecursively()
+    }
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
