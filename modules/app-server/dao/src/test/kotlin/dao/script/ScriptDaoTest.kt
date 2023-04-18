29003b158ec24cd5b6cfd327cf0f52de06e88f73
asolovieff <alexandre.solovieff@4sh.fr>
2023-04-18 18:48:58 +0200

    feat+test: Implement ScriptDao and its TU

---
+++ modules/app-server/dao/src/test/kotlin/dao/script/ScriptDaoTest.kt
@@ -0,0 +1,208 @@
+package dao.script
+
+import AbstractDaoTest
+import jooq.generated.domain.tables.pojos.DmScript
+import jooq.generated.domain.tables.references.DM_SCRIPT
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+import strikt.assertions.isNotNull
+import strikt.assertions.isNull
+import java.util.UUID
+
+class ScriptDaoTest : AbstractDaoTest() {
+    private val scriptDao = ScriptDao(dslContext)
+
+    override fun dropTables() {
+        dslContext.delete(DM_SCRIPT).execute()
+    }
+
+    @Nested
+    inner class TestInsert {
+        @Test
+        fun `insert should return inserted document`() {
+            // Given
+            val dmScript = buildDmScript()
+
+            // When
+            val insertedScript = scriptDao.insert(dmScript)
+
+            // Then
+            expectThat(insertedScript).isNotNull().and {
+                get { name }.isEqualTo(dmScript.name)
+                get { checksum }.isEqualTo(dmScript.checksum)
+                get { content }.isEqualTo(dmScript.content)
+            }
+        }
+
+        @Test
+        fun `insert should write document in database`() {
+            // Given
+            val scriptName = "myName"
+            val dmScript = buildDmScript(name = scriptName)
+
+            // When
+            val insertedId = scriptDao.insert(dmScript)?.checksum
+
+            // Then
+            val insertedDmScript = dslContext.select(DM_SCRIPT.NAME, DM_SCRIPT.CHECKSUM, DM_SCRIPT.CONTENT)
+                .from(DM_SCRIPT)
+                .where(DM_SCRIPT.CHECKSUM.eq(insertedId))
+                .fetchOneInto(DmScript::class.java)
+
+            expectThat(insertedDmScript).isNotNull().and {
+                get { name }.isEqualTo(scriptName)
+                get { checksum }.isEqualTo(dmScript.checksum)
+                get { content }.isEqualTo(dmScript.content)
+            }
+        }
+    }
+
+    @Nested
+    inner class TestFindOneById {
+        @Test
+        fun `should return null when checksum does not exist in db`() {
+            // Given
+            val checksum = UUID.randomUUID().toString()
+
+            // When
+            val loadedScript = scriptDao.findOneById(checksum)
+
+            // Then
+            expectThat(loadedScript).isNull()
+        }
+
+        @Test
+        fun `should load project from db when it exists`() {
+            // Given
+            val script = buildDmScript()
+            val insertedChecksum = scriptDao.insert(script)!!.checksum!!
+
+            // When
+            val loadedScript = scriptDao.findOneById(insertedChecksum)
+
+            // Then
+            expectThat(loadedScript).isNotNull().and {
+                get { name }.isEqualTo(script.name)
+                get { checksum }.isEqualTo(script.checksum)
+                get { content }.isEqualTo(script.content)
+            }
+        }
+    }
+
+    @Nested
+    inner class TestUpdate {
+        @Test
+        fun `should return null when id does not exist in db`() {
+            // Given
+            val script = buildDmScript(name = "myName", content = "myContent")
+            scriptDao.insert(script)
+            val randomChecksum = UUID.randomUUID().toString()
+
+            // When
+            val updatedScript = scriptDao.update(buildDmScript(
+                name = "myOtherName",
+                checksum = randomChecksum,
+                content = "myOtherContent"
+            ))
+
+            // Then
+            expectThat(updatedScript).isNull()
+        }
+
+        @Test
+        fun `should not update anything when id does not exist`() {
+            // Given
+            val script = buildDmScript(name = "myName", content = "myContent")
+            val insertedChecksum = scriptDao.insert(script)!!.checksum!!
+            val randomChecksum = UUID.randomUUID().toString()
+
+            // When
+            scriptDao.update(buildDmScript(
+                name = "myOtherName",
+                content = "myOtherContent",
+                checksum = randomChecksum
+            ))
+
+            // Then
+            expectThat(scriptDao.findOneById(insertedChecksum)).isNotNull().and {
+                get { name }.isEqualTo(script.name)
+                get { content }.isEqualTo(script.content)
+                get { checksum }.isEqualTo(script.checksum)
+            }
+        }
+
+        @Test
+        fun `should update given project`() {
+            // Given
+            val script = buildDmScript(name = "myName", content = "myContent")
+            val insertedChecksum = scriptDao.insert(script)!!.checksum!!
+
+            // When
+            val newName = "myOtherName"
+            val newContent = "myOtherContent"
+            scriptDao.update(buildDmScript(checksum = insertedChecksum, name = newName, content = newContent))
+
+            // Then
+            expectThat(scriptDao.findOneById(insertedChecksum)).isNotNull().and {
+                get { checksum }.isEqualTo(insertedChecksum)
+                get { name }.isEqualTo(newName)
+                get { content }.isEqualTo(newContent)
+            }
+        }
+
+        @Test
+        fun `should return updated project`() {
+            // Given
+            val script = buildDmScript(name = "myName", content = "myContent")
+            val insertedChecksum = scriptDao.insert(script)!!.checksum!!
+
+            // When
+            val newName = "myOtherName"
+            val newContent = "myOtherContent"
+            val updatedScript = scriptDao.update(
+                buildDmScript(checksum = insertedChecksum, name = newName, content = newContent)
+            )
+
+            // Then
+            expectThat(updatedScript).isNotNull().and {
+                get { checksum }.isEqualTo(insertedChecksum)
+                get { name }.isEqualTo(newName)
+                get { content }.isEqualTo(newContent)
+            }
+        }
+    }
+
+    @Nested
+    inner class TestDelete {
+        @Test
+        fun `should do nothing when deleting non existing document`() {
+            // Given
+            val insertedChecksum = scriptDao.insert(buildDmScript())!!.checksum!!
+            val randomChecksum = UUID.randomUUID().toString()
+
+            // When
+            scriptDao.delete(randomChecksum)
+
+            // Then
+            expectThat(scriptDao.findOneById(insertedChecksum)).isNotNull()
+        }
+
+        @Test
+        fun `should delete proper document`() {
+            // Given
+            val insertedChecksum1 =
+                scriptDao.insert(buildDmScript(checksum = UUID.randomUUID().toString()))!!.checksum!!
+            val insertedChecksum2 =
+                scriptDao.insert(buildDmScript(checksum = UUID.randomUUID().toString()))!!.checksum!!
+
+            // When
+            scriptDao.delete(insertedChecksum1)
+
+            // Then
+            expectThat(scriptDao.findOneById(insertedChecksum1)).isNull()
+            expectThat(scriptDao.findOneById(insertedChecksum2)).isNotNull()
+        }
+    }
+}
