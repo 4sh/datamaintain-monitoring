94493e67a465f7c0303d230f1a31338fb1d27bdd
Élise Rubio <elise.rubio@4sh.fr>
2023-04-18 17:13:11 +0200

    feat: Method to Insert environment in db

--- modules/app-server/dao/src/main/kotlin/dao/environment/EnvironmentDao.kt
+++ modules/app-server/dao/src/main/kotlin/dao/environment/EnvironmentDao.kt
@@ -2,13 +2,22 @@ package dao.environment
 
 import dao.BaseDao
 import jooq.generated.domain.tables.pojos.DmEnvironment
+import jooq.generated.domain.tables.references.DM_ENVIRONMENT
 import org.jooq.DSLContext
+import org.jooq.impl.DSL.defaultValue
+import org.jooq.impl.DSL.`val`
 import java.util.*
 
 class EnvironmentDao(override val dslContext: DSLContext): BaseDao<DmEnvironment, UUID> {
-    override fun insert(data: DmEnvironment): DmEnvironment? {
-        TODO("Not yet implemented")
-    }
+    override fun insert(data: DmEnvironment): DmEnvironment? =
+        dslContext.insertInto(DM_ENVIRONMENT, DM_ENVIRONMENT.ID, DM_ENVIRONMENT.NAME, DM_ENVIRONMENT.FK_PROJECT_REF)
+            .values(
+                defaultValue(DM_ENVIRONMENT.ID),
+                `val`(data.name),
+                `val`(data.fkProjectRef)
+            ).returningResult(DM_ENVIRONMENT.ID, DM_ENVIRONMENT.NAME, DM_ENVIRONMENT.FK_PROJECT_REF)
+            .fetchOne()
+            ?.into(DmEnvironment::class.java)
 
     override fun update(data: DmEnvironment): DmEnvironment? {
         TODO("Not yet implemented")
