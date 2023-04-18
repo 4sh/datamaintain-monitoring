17fbaddf12164d0e5f74d8c92b2dd2db524465f0
asolovieff <alexandre.solovieff@4sh.fr>
2023-04-18 14:24:58 +0200

    feat: Add AppModule

--- modules/app-server/app/build.gradle.kts
+++ modules/app-server/app/build.gradle.kts
@@ -8,4 +8,8 @@ dependencies {
     implementation(project(":modules:app-server:rest"))
 
     implementation("io.ktor:ktor-server-netty:2.2.4")
+    implementation("org.jooq:jooq:3.18.2")
+    implementation(project(":modules:app-server:dao"))
+    implementation(project(":modules:app-server:domain"))
+    implementation("org.postgresql:postgresql:42.5.1")
 }
---
+++ modules/app-server/app/src/main/kotlin/AppModule.kt
@@ -0,0 +1,20 @@
+import dao.project.ProjectDao
+import jooq.generated.domain.tables.pojos.DmProject
+import org.jooq.SQLDialect
+import org.jooq.impl.DSL
+import java.sql.Connection
+import java.sql.DriverManager
+import java.util.*
+
+val url = "jdbc:postgresql://localhost:5432/datamaintain"
+val user = "datamaintain"
+val mdp = "datamaintain"
+
+val connection: Connection = DriverManager.getConnection(url, user, mdp)
+val dslContext = DSL.using(connection, SQLDialect.POSTGRES)
+
+val projectDao = ProjectDao(dslContext)
+
+fun main() {
+    //println(projectDao.insert(DmProject(name = "Izivia")))
+}
