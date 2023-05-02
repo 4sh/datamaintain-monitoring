e10d91db2639e97ec3a99a6bceb8bfd4850670d1
asolovieff <alexandre.solovieff@4sh.fr>
2023-05-02 17:07:46 +0200

    feat+test: Implement ScriptExecutionTagDao and its TU

---
+++ modules/app-server/dao/src/main/kotlin/dao/script/execution/tag/ScriptExecutionTagDao.kt
@@ -0,0 +1,30 @@
+package dao.script.execution.tag
+
+import jooq.generated.domain.tables.pojos.DmScriptExecutionDmTag
+import jooq.generated.domain.tables.references.DM_SCRIPT_EXECUTION_DM_TAG
+import org.jooq.DSLContext
+import org.jooq.impl.DSL.`val`
+import java.util.*
+
+class ScriptExecutionTagDao(val dslContext: DSLContext) {
+    fun insert(data: DmScriptExecutionDmTag): DmScriptExecutionDmTag? =
+        dslContext.insertInto(
+            DM_SCRIPT_EXECUTION_DM_TAG,
+            DM_SCRIPT_EXECUTION_DM_TAG.FK_SCRIPT_EXECUTION_REF,
+            DM_SCRIPT_EXECUTION_DM_TAG.FK_TAG_REF
+        )
+            .values(
+                `val`(data.fkScriptExecutionRef),
+                `val`(data.fkTagRef)
+            )
+            .returningResult(DM_SCRIPT_EXECUTION_DM_TAG.FK_SCRIPT_EXECUTION_REF, DM_SCRIPT_EXECUTION_DM_TAG.FK_TAG_REF)
+            .fetchOne()
+            ?.into(DmScriptExecutionDmTag::class.java)
+
+    fun delete(scriptExecutionRef: UUID, tagRef: String) {
+        dslContext.delete(DM_SCRIPT_EXECUTION_DM_TAG)
+            .where(DM_SCRIPT_EXECUTION_DM_TAG.FK_SCRIPT_EXECUTION_REF.eq(scriptExecutionRef))
+            .and(DM_SCRIPT_EXECUTION_DM_TAG.FK_TAG_REF.eq(tagRef))
+            .execute()
+    }
+}
