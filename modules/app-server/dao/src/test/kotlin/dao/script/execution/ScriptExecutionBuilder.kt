bfc45ac3e4d6613a3227e00ac4718dcfbcd572bc
Élise Rubio <elise.rubio@4sh.fr>
2023-05-02 15:37:32 +0200

    feat+test: Implement ScriptExecutionDao and its TU

---
+++ modules/app-server/dao/src/test/kotlin/dao/script/execution/ScriptExecutionBuilder.kt
@@ -0,0 +1,27 @@
+package dao.script.execution
+
+import jooq.generated.domain.enums.ScriptExecutionStatus
+import jooq.generated.domain.tables.pojos.DmScriptExecution
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+import java.util.*
+
+fun buildDmScriptExecution(
+    id: UUID? = null,
+    startDate: OffsetDateTime = OffsetDateTime.of(2023, 5, 2, 14, 26, 0, 0, ZoneOffset.UTC),
+    endDate: OffsetDateTime? = null,
+    durationInMs: Int? = null,
+    output: String? = null,
+    status: ScriptExecutionStatus? = null,
+    fkScriptRef: String,
+    fkBatchExecutionRef: UUID
+) = DmScriptExecution(
+    id = id,
+    startDate = startDate,
+    endDate = endDate,
+    durationInMs = durationInMs,
+    output = output,
+    status = status,
+    fkScriptRef = fkScriptRef,
+    fkBatchExecutionRef = fkBatchExecutionRef,
+)
