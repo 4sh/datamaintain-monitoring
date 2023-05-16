d7703a2423789676211f25a72fddf4c96d08b767
asolovieff <alexandre.solovieff@4sh.fr>
2023-05-16 15:16:51 +0200

    refacto: Remove BaseDao and its usages

--- modules/app-server/dao/src/main/kotlin/dao/environment/EnvironmentDao.kt
+++ modules/app-server/dao/src/main/kotlin/dao/environment/EnvironmentDao.kt
@@ -1,6 +1,5 @@
 package dao.environment
 
-import dao.BaseDao
 import jooq.generated.domain.tables.pojos.DmEnvironment
 import jooq.generated.domain.tables.references.DM_ENVIRONMENT
 import org.jooq.DSLContext
@@ -8,8 +7,8 @@ import org.jooq.impl.DSL.defaultValue
 import org.jooq.impl.DSL.`val`
 import java.util.*
 
-class EnvironmentDao(override val dslContext: DSLContext): BaseDao<DmEnvironment, UUID> {
-    override fun insert(data: DmEnvironment): DmEnvironment? =
+class EnvironmentDao(val dslContext: DSLContext) {
+    fun insert(data: DmEnvironment): DmEnvironment? =
         dslContext.insertInto(DM_ENVIRONMENT, DM_ENVIRONMENT.ID, DM_ENVIRONMENT.NAME, DM_ENVIRONMENT.FK_PROJECT_REF)
             .values(
                 defaultValue(DM_ENVIRONMENT.ID),
@@ -19,7 +18,7 @@ class EnvironmentDao(override val dslContext: DSLContext): BaseDao<DmEnvironment
             .fetchOne()
             ?.into(DmEnvironment::class.java)
 
-    override fun update(data: DmEnvironment): DmEnvironment? =
+    fun update(data: DmEnvironment): DmEnvironment? =
         dslContext.update(DM_ENVIRONMENT)
             .set(DM_ENVIRONMENT.NAME, data.name)
             .where(DM_ENVIRONMENT.ID.eq(data.id))
@@ -27,13 +26,13 @@ class EnvironmentDao(override val dslContext: DSLContext): BaseDao<DmEnvironment
             .fetchOne()
             ?.into(DmEnvironment::class.java)
 
-    override fun delete(id: UUID) {
+    fun delete(id: UUID) {
         dslContext.delete(DM_ENVIRONMENT)
             .where(DM_ENVIRONMENT.ID.eq(id))
             .execute()
     }
 
-    override fun findOneById(id: UUID): DmEnvironment? =
+    fun findOneById(id: UUID): DmEnvironment? =
         dslContext.fetchOne(DM_ENVIRONMENT, DM_ENVIRONMENT.ID.eq(id))
             ?.into(DmEnvironment::class.java)
 }
--- modules/app-server/dao/src/main/kotlin/dao/module/ModuleDao.kt
+++ modules/app-server/dao/src/main/kotlin/dao/module/ModuleDao.kt
@@ -1,6 +1,5 @@
 package dao.module
 
-import dao.BaseDao
 import jooq.generated.domain.tables.pojos.DmModule
 import jooq.generated.domain.tables.references.DM_MODULE
 import org.jooq.DSLContext
@@ -8,8 +7,8 @@ import org.jooq.impl.DSL.defaultValue
 import org.jooq.impl.DSL.`val`
 import java.util.UUID
 
-class ModuleDao(override val dslContext: DSLContext) : BaseDao<DmModule, UUID> {
-    override fun insert(data: DmModule): DmModule? =
+class ModuleDao(val dslContext: DSLContext) {
+    fun insert(data: DmModule): DmModule? =
         dslContext.insertInto(DM_MODULE, DM_MODULE.ID, DM_MODULE.NAME, DM_MODULE.FK_PROJECT_REF)
             .values(
                 defaultValue(DM_MODULE.ID),
@@ -19,7 +18,7 @@ class ModuleDao(override val dslContext: DSLContext) : BaseDao<DmModule, UUID> {
             .fetchOne()
             ?.into(DmModule::class.java)
 
-    override fun update(data: DmModule): DmModule? =
+    fun update(data: DmModule): DmModule? =
         dslContext.update(DM_MODULE)
             .set(DM_MODULE.NAME, data.name)
             .where(DM_MODULE.ID.eq(data.id))
@@ -27,13 +26,13 @@ class ModuleDao(override val dslContext: DSLContext) : BaseDao<DmModule, UUID> {
             .fetchOne()
             ?.into(DmModule::class.java)
 
-    override fun delete(id: UUID) {
+    fun delete(id: UUID) {
         dslContext.delete(DM_MODULE)
             .where(DM_MODULE.ID.eq(id))
             .execute()
     }
 
-    override fun findOneById(id: UUID): DmModule? =
+    fun findOneById(id: UUID): DmModule? =
         dslContext.fetchOne(DM_MODULE, DM_MODULE.ID.eq(id))
             ?.into(DmModule::class.java)
 }
