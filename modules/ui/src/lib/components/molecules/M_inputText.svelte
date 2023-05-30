934e3cff70b06229dc0e73b8faecfb6b1e2a905d
driccio <damien.riccio@4sh.fr>
2023-05-30 17:18:17 +0200

    feat: enrich component

--- modules/ui/src/lib/components/molecules/M_inputText.svelte
+++ modules/ui/src/lib/components/molecules/M_inputText.svelte
@@ -4,9 +4,10 @@
 
     export let label;
     export let value;
+    export let required;
     export let placeholder;
 </script>
 
 <A_label {label}>
-    <A_inputText {value} {placeholder}></A_inputText>
+    <A_inputText {value} {required} {placeholder}></A_inputText>
 </A_label>
--- modules/ui/src/routes/projects/[project]/edit/+page.svelte
+++ modules/ui/src/routes/projects/[project]/edit/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import {ProjectService} from "$lib/services/ProjectService";
     import {page} from "$app/stores";
+    import O_projectEdition from "$lib/components/organisms/O_project/O_projectEdition.svelte";
 
     let projectPromise
 
@@ -13,6 +14,10 @@
     <p>...waiting</p>
 {:then project}
     Edition du project {project.name}
+
+
+    <O_projectEdition {project} />
+
 {:catch error}
     <p style="color: red">Project not found !</p>
 {/await}
