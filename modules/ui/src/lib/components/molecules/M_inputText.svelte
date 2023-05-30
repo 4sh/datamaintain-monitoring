ee2b64b8853ea4319278a5eea48a5c42e7c76b7f
driccio <damien.riccio@4sh.fr>
2023-05-30 15:35:36 +0200

    feat: add input and label atoms/molecules

---
+++ modules/ui/src/lib/components/molecules/M_inputText.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import A_label from "$lib/components/atoms/A_label.svelte";
+    import A_inputText from "$lib/components/atoms/form/A_inputText.svelte";
+
+    export let label;
+    export let value;
+</script>
+
+<A_label {label}>
+    <A_inputText {value}></A_inputText>
+</A_label>
---
+++ modules/ui/src/lib/components/organisms/O_project/0_projectEdition.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import M_inputText from "$lib/components/molecules/M_inputText.svelte";
+
+    export let project = {}
+</script>
+
+<form>
+    <M_inputText label="Name" bind:value={project.name} />
+</form>
+
+<style>
+</style>
