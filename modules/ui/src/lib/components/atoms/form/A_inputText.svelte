b98595bf56a53922565825faff9be2968ee2eda0
wandabardey <wanda.bardey@4sh.fr>
2023-05-31 15:32:18 +0200

    skin input text and label components

--- modules/ui/src/lib/components/atoms/form/A_inputText.svelte
+++ modules/ui/src/lib/components/atoms/form/A_inputText.svelte
@@ -1,5 +1,46 @@
 <script lang="ts">
     export let value;
+    export let placeholder;
 </script>
 
-<input type="text" {value}>
+<input type="text" {value} placeholder="{placeholder}" class="aInputText">
+
+<style lang="scss">
+    @import "src/app";
+
+    .aInputText {
+      background-color: transparent !important;
+      border-top: none;
+      border-left: none;
+      border-right: none;
+      border-radius: 0;
+      border-bottom: 0.5px solid $text-color;
+      color: $secondary-color;
+      font-size: rem-calc(13px)!important;
+      line-height: rem-calc(16px)!important;
+      margin: 0;
+      font-weight: 400;
+      padding: 0;
+      max-height: rem-calc(17px);
+      width: 100%;
+      outline: none;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      caret-color: $text-color;
+
+      &:focus {
+        box-shadow: none;
+      }
+
+      &::placeholder {
+        font-weight: 400;
+        font-style: italic;
+        color: rgb(var(--secondary-color-rgb) / .5);
+      }
+
+      &[readonly] {
+        cursor: pointer!important;
+      }
+    }
+</style>
