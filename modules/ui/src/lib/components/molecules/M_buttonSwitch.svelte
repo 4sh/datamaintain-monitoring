33f214e25855362f52eea9f0df62b69ea4088a01
wandabardey <wanda.bardey@4sh.fr>
2023-05-31 17:41:30 +0200

    skin switch button for dark mode

---
+++ modules/ui/src/lib/components/molecules/M_buttonSwitch.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import A_label from "$lib/components/atoms/A_label.svelte";
+    import A_buttonSwitch from "$lib/components/atoms/A_buttonSwitch.svelte";
+
+    export let checked;
+    export let label;
+    export let alignment;
+    export let flex;
+</script>
+
+<A_label {label} {alignment} {flex}>
+    <A_buttonSwitch bind:checked={checked}></A_buttonSwitch>
+</A_label>
