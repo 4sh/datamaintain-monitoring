b0a45f123e8492bd0e2eb77ff6aec0ddcfedeccc
wandabardey <wanda.bardey@4sh.fr>
2023-06-13 18:15:03 +0200

    start alert skin

---
+++ modules/ui/src/lib/components/molecules/M_alertItem.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+
+import A_icon from "$lib/components/atoms/A_icon.svelte";
+import A_indicator from "$lib/components/atoms/A_indicator.svelte";
+</script>
+
+<div class="alertItem">
+    <div class="alertItem-icon">
+        <A_icon type="warning" size="light"></A_icon>
+    </div>
+    <div class="alertItem-content">
+        <div class="alertItem-title">PF12092022-1</div>
+        <div class="alertItem-info">Lorem ipsum dolor sit amet, consectetur</div>
+    </div>
+    <div class="alertItem-shortName">
+        <A_indicator label="PoF"></A_indicator>
+    </div>
+    <div class="alertItem-icon">
+        <A_icon type="notifications_none" size="light"></A_icon>
+    </div>
+</div>
+
+<style lang="scss">
+  @import "src/app";
+
+  .alertItem {
+    display: flex;
+    align-items: center;
+
+    &-content {
+
+    }
+
+    &-title {
+      font-size: rem-calc(14px);
+    }
+
+    &-info {
+      font-size: rem-calc(12px);
+      font-weight: 300;
+    }
+  }
+
+</style>
---
+++ modules/ui/src/lib/components/organisms/O_alertList.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+
+import {Svroller} from "svrollbar";
+import M_alertItem from "$lib/components/molecules/M_alertItem.svelte";
+</script>
+
+<div class="alertList">
+    <div class="alertList-header">
+        <div class="alertList-header-title">
+            Toutes les alertes
+        </div>
+        <div class="alertList-header-info">
+            Il y a 7 alertes sur vos projets qui n’ont pas été acquittées
+        </div>
+    </div>
+    <div class="alertList-content">
+        <Svroller>
+           <M_alertItem></M_alertItem>
+           <M_alertItem></M_alertItem>
+           <M_alertItem></M_alertItem>
+           <M_alertItem></M_alertItem>
+           <M_alertItem></M_alertItem>
+           <M_alertItem></M_alertItem>
+           <M_alertItem></M_alertItem>
+           <M_alertItem></M_alertItem>
+           <M_alertItem></M_alertItem>
+           <M_alertItem></M_alertItem>
+           <M_alertItem></M_alertItem>
+           <M_alertItem></M_alertItem>
+           <M_alertItem></M_alertItem>
+        </Svroller>
+    </div>
+</div>
+
+<style lang="scss">
+  @import "src/app";
+
+  .alertList {
+    background-color: rgb(var(--secondary-color-rgb) / .06);
+    border-radius: rem-calc(6px);
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+
+    &-header {
+      background-color: $app-primary_900;
+      border-radius: rem-calc(6px);
+      padding: rem-calc(20px 25px);
+      flex: 0 0 auto;
+
+      &-title {
+        font-size: rem-calc(16px);
+      }
+      &-info {
+        font-size: rem-calc(11px);
+        font-weight: 300;
+        margin-top: rem-calc(5px);
+      }
+    }
+
+    &-content {
+      padding: rem-calc(25px 10px 25px 25px);
+      flex: 1 1 0;
+      overflow: hidden;
+    }
+  }
+
+</style>
