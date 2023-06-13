f117fbaa839a3396de57457abf3c5c0a22ad24cc
Élise Rubio <elise.rubio@4sh.fr>
2023-06-13 16:06:17 +0200

    :truck: Move jooq domain to dao module

--- modules/app-server/dao/build.gradle.kts
+++ modules/app-server/dao/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm")
+    id("nu.studer.jooq") version "8.1"
 }
 
 repositories {
@@ -7,10 +8,13 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("stdlib-jdk8"))
     implementation("org.jooq:jooq:3.18.2")
     implementation(project(":modules:app-server:domain"))
+    implementation("org.postgresql:postgresql:42.5.1")
 
-    testImplementation("org.postgresql:postgresql:42.5.1")
+    jooqGenerator("org.postgresql:postgresql:42.5.1")
+    implementation("jakarta.validation:jakarta.validation-api:3.0.2")
 
     testImplementation("io.strikt:strikt-core:0.34.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
@@ -20,6 +24,69 @@ dependencies {
     testImplementation("org.testcontainers:testcontainers:1.18.0")
     testImplementation("org.testcontainers:junit-jupiter:1.18.0")
     testImplementation("org.testcontainers:postgresql:1.18.0")
+}
+
+jooq {
+    version.set("3.18.2")  // default (can be omitted)
+    edition.set(nu.studer.gradle.jooq.JooqEdition.OSS)  // default (can be omitted)
+
+    configurations {
+        create("main") {  // name of the jOOQ configuration
+            generateSchemaSourceOnCompilation.set(true)  // default (can be omitted)
+
+            jooqConfiguration.apply {
+                logging = org.jooq.meta.jaxb.Logging.WARN
+                jdbc.apply {
+                    driver = "org.postgresql.Driver"
+                    url = "jdbc:postgresql://localhost:5432/datamaintain"
+                    user = "datamaintain"
+                    password = "datamaintain"
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.KotlinGenerator"
+                    database.apply {
+                        name = "org.jooq.meta.postgres.PostgresDatabase"
+                        inputSchema = "public"
+                    }
+                    generate.apply {
+                        isDeprecated = false
+                        isRecords = true
+                        isImmutablePojos = true
+                        isFluentSetters = true
+                        isImplicitJoinPathsAsKotlinProperties = true
+                        isKotlinSetterJvmNameAnnotationsOnIsPrefix = true
+                        isPojosAsKotlinDataClasses = true
+                        isValidationAnnotations = true
+                    }
+                    target.apply {
+                        packageName = "generated.domain"
+                        directory = "${project.rootDir}/modules/app-server/dao/src/jooq"
+                    }
+                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+                }
+            }
+        }
+    }
+}
+
+buildscript {
+    configurations["classpath"].resolutionStrategy.eachDependency {
+        if (requested.group == "org.jooq") {
+            useVersion("3.18.2")
+        }
+    }
+}
+
+tasks.named<nu.studer.gradle.jooq.JooqGenerate>("generateJooq") {
+    (launcher::set)(javaToolchains.launcherFor {
+        languageVersion.set(JavaLanguageVersion.of(18))
+    })
+}
+
+tasks.named("clean") {
+    doLast {
+        file("src/jooq").deleteRecursively()
+    }
 }
 
 tasks.getByPath("test").doFirst {
