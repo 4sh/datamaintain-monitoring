ec01a21882f09eb9ec0b6f7f6175a0229dee03bb
asolovieff <alexandre.solovieff@4sh.fr>
2023-06-13 18:10:15 +0200

    refacto: Simplify ktor usage

--- modules/api-execution-report/rest/src/main/kotlin/api/execution/report/rest/Application.kt
+++ modules/api-execution-report/rest/src/main/kotlin/api/execution/report/rest/Application.kt
@@ -1,7 +1,7 @@
 package api.execution.report.rest
 
 import api.execution.report.client.HelloWorldClient
-import api.execution.report.rest.v1.RouteV1
+import api.execution.report.rest.v1.routeV1
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
@@ -10,7 +10,7 @@ import io.ktor.server.routing.*
 fun Application.configureRouting(client: HelloWorldClient) {
     routing {
         route("/api") {
-            RouteV1(client).routeV1(this)
+            routeV1(client)
         }
     }
 }
