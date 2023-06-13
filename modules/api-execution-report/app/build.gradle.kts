269db37378f1c9943b1236b45b46040ad743d9ba
asolovieff <alexandre.solovieff@4sh.fr>
2023-06-13 17:33:24 +0200

    feat: Setup all api-execution-report inside AppServer.kt

--- modules/api-execution-report/app/build.gradle.kts
+++ modules/api-execution-report/app/build.gradle.kts
@@ -6,4 +6,11 @@ appProject()
 
 dependencies {
     implementation(project(":modules:api-execution-report:rest"))
+    implementation(project(":modules:api-execution-report:client"))
+    implementation(project(":modules:proto"))
+
+    implementation("io.grpc:grpc-protobuf:1.55.1")
+
+    implementation("io.ktor:ktor-server-core:2.2.4")
+    implementation("io.ktor:ktor-server-netty:2.2.4")
 }
