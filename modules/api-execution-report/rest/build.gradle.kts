662725868dc230e644ab105dd5761dcb43292571
asolovieff <alexandre.solovieff@4sh.fr>
2023-06-13 17:43:21 +0200

    fix: Remove useless commented dependencies

--- modules/api-execution-report/rest/build.gradle.kts
+++ modules/api-execution-report/rest/build.gradle.kts
@@ -19,10 +19,5 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.2.4")
     implementation("ch.qos.logback:logback-classic:1.4.7")
 
-//    implementation("io.grpc:grpc-kotlin-stub:1.3.0")
     implementation("io.grpc:grpc-protobuf:1.55.1")
-//    implementation("com.google.protobuf:protobuf-java-util:3.22.0")
-//    implementation("com.google.protobuf:protobuf-kotlin:3.23.0")
-
-//    runtimeOnly("io.grpc:grpc-netty:1.46.0")
 }
