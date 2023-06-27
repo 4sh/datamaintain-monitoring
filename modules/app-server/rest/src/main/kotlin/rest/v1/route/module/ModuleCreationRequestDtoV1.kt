64d3d1414a1de63a1b08a37ef9e4919b8f330b22
asolovieff <alexandre.solovieff@4sh.fr>
2023-06-27 18:36:04 +0200

    feat: Add ModuleDtoV1 and dtos for requests

---
+++ modules/app-server/rest/src/main/kotlin/rest/v1/route/module/ModuleCreationRequestDtoV1.kt
@@ -0,0 +1,18 @@
+package rest.v1.route.module
+
+import kotlinx.serialization.Serializable
+import module.ModuleCreationRequest
+import rest.UUIDSerializer
+import java.util.*
+
+@Serializable
+data class ModuleCreationRequestDtoV1(
+    val name: String,
+    @Serializable(with = UUIDSerializer::class)
+    val fkProjectRef: UUID
+)
+
+fun ModuleCreationRequestDtoV1.toDomain(): ModuleCreationRequest = ModuleCreationRequest(
+    name = this.name,
+    fkProjectRef = this.fkProjectRef
+)
---
+++ modules/app-server/rest/src/main/kotlin/rest/v1/route/module/ModuleDtoV1.kt
@@ -0,0 +1,18 @@
+package rest.v1.route.module
+
+import kotlinx.serialization.Serializable
+import module.Module
+import rest.UUIDSerializer
+import java.util.UUID
+
+@Serializable
+data class ModuleDtoV1(
+    @Serializable(with = UUIDSerializer::class)
+    val id: UUID,
+    val name: String
+)
+
+fun Module.toDtoV1() = ModuleDtoV1(
+    id = this.id,
+    name = this.name
+)
