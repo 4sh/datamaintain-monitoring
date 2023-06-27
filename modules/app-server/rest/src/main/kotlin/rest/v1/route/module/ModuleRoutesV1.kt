1cd97863eded15c4a29523e912dd23973371359f
asolovieff <alexandre.solovieff@4sh.fr>
2023-06-27 18:36:34 +0200

    feat: Add ModuleRoutesV1

---
+++ modules/app-server/rest/src/main/kotlin/rest/v1/route/module/ModuleRoutesV1.kt
@@ -0,0 +1,30 @@
+package rest.v1.route.module
+
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import module.ModuleService
+import java.util.*
+
+internal const val moduleId = "moduleId"
+
+internal fun ApplicationCall.moduleId() = UUID.fromString(this.parameters[moduleId])
+
+internal fun Route.moduleV1Routes(moduleService: ModuleService) {
+    route("/modules") {
+        get("{$moduleId}") {
+            call.respondNullable(moduleService.findOneById(call.moduleId())?.toDtoV1())
+        }
+
+        post {
+            val moduleCreationRequest = call.receive<ModuleCreationRequestDtoV1>().toDomain()
+            call.respond(moduleService.insert(moduleCreationRequest).toDtoV1())
+        }
+
+        put("{$moduleId}/name") {
+            val moduleNameUpdateRequest = call.receive<ModuleNameUpdateRequestDtoV1>().toDomain()
+            call.respondNullable(moduleService.updateModuleName(call.moduleId(), moduleNameUpdateRequest)?.toDtoV1())
+        }
+    }
+}
