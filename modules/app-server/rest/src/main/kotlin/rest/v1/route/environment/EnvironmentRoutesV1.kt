2e8e2959cbf6880f7495813854daba82dcd99fd9
asolovieff <alexandre.solovieff@4sh.fr>
2023-07-11 14:53:59 +0200

    feat: Add EnvironmentRoutesV1

---
+++ modules/app-server/rest/src/main/kotlin/rest/v1/route/environment/EnvironmentRoutesV1.kt
@@ -0,0 +1,30 @@
+package rest.v1.route.environment
+
+import environment.EnvironmentService
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.util.*
+
+internal  const val environmentId = "environmentId"
+
+internal fun ApplicationCall.environmentId() = UUID.fromString(this.parameters[environmentId])
+
+internal fun Route.environmentV1Routes(environmentService: EnvironmentService) {
+    route("/environments") {
+        get("{$environmentId}") {
+            call.respondNullable(environmentService.findOneById(call.environmentId())?.toDtoV1())
+        }
+
+        post {
+            val environmentCreationRequest = call.receive<EnvironmentCreationRequestDtoV1>().toDomain()
+            call.respond(environmentService.insert(environmentCreationRequest).toDtoV1())
+        }
+
+        put("{$environmentId}/name") {
+            val environmentNameUpdateRequest = call.receive<EnvironmentNameUpdateRequestDtoV1>().toDomain()
+            call.respondNullable(environmentService.updateEnvironmentName(call.environmentId(), environmentNameUpdateRequest))
+        }
+    }
+}
