93eba15efe5025901ede92652f2cfe42f6bf3a60
Élise Rubio <elise.rubio@4sh.fr>
2023-08-22 16:58:04 +0200

    :wrench: Add proxy to server

--- modules/ui/vite.config.js
+++ modules/ui/vite.config.js
@@ -3,6 +3,11 @@ import {viteStaticCopy} from "vite-plugin-static-copy";
 
 /** @type {import('vite').UserConfig} */
 const config = {
+	server: {
+		proxy: {
+			'/api': 'http://localhost:8080'
+		}
+	},
 	plugins: [
 		sveltekit(),
 		viteStaticCopy({
