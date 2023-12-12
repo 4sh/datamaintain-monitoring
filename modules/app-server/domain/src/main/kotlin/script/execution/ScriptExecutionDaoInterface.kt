ae4a01dc29b31c41a02d6b83d1f15b8db37cd8b2
asolovieff <alexandre.solovieff@4sh.fr>
2023-12-12 16:41:02 +0100

    feat: Add ScriptExecutionDaoInterface

---
+++ modules/app-server/domain/src/main/kotlin/script/execution/ScriptExecutionDaoInterface.kt
@@ -0,0 +1,17 @@
+package script.execution
+
+import java.util.*
+
+interface ScriptExecutionDaoInterface {
+    fun insert(data: ScriptExecutionCreationRequest): ScriptExecution
+    fun updateScriptExecutionStartData(
+        scriptExecutionId: UUID,
+        executionStartData: ScriptExecutionStartUpdateRequest
+    ): ScriptExecution?
+    fun updateScriptExecutionEndData(
+        scriptExecutionId: UUID,
+        executionEndData: ScriptExecutionEndUpdateRequest
+    ): ScriptExecution?
+    fun delete(id: UUID)
+    fun findOneById(id: UUID): ScriptExecution?
+}
