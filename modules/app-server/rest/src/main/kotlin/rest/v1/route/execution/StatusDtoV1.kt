90be461d72c9ff6d032d36bba8f1b6daafc33db4
asolovieff <alexandre.solovieff@4sh.fr>
2023-12-12 11:49:53 +0100

    feat: Add DtoV1 for BatchExecution and its enums

---
+++ modules/app-server/rest/src/main/kotlin/rest/v1/route/execution/StatusDtoV1.kt
@@ -0,0 +1,18 @@
+package rest.v1.route.execution
+
+import execution.Status
+
+enum class StatusDtoV1 {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED,
+    ERROR
+}
+
+fun Status.toDtoV1() =
+    when (this) {
+        Status.PENDING -> StatusDtoV1.PENDING
+        Status.IN_PROGRESS -> StatusDtoV1.IN_PROGRESS
+        Status.COMPLETED -> StatusDtoV1.COMPLETED
+        Status.ERROR -> StatusDtoV1.ERROR
+    }
---
+++ modules/app-server/rest/src/main/kotlin/rest/v1/route/execution/batch/OriginDtoV1.kt
@@ -0,0 +1,16 @@
+package rest.v1.route.execution.batch
+
+import execution.batch.BatchExecution
+
+enum class OriginDtoV1 {
+    CLIENT,
+    SERVER,
+    TIER
+}
+
+fun BatchExecution.Origin.toDtoV1() =
+    when (this) {
+        BatchExecution.Origin.CLIENT -> OriginDtoV1.CLIENT
+        BatchExecution.Origin.SERVER -> OriginDtoV1.SERVER
+        BatchExecution.Origin.TIER -> OriginDtoV1.TIER
+    }
