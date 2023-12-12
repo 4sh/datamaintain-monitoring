88e7632481955854be02fb14f2bf2a5041c91be3
asolovieff <alexandre.solovieff@4sh.fr>
2023-12-12 11:49:07 +0100

    feat: Add OffsetDateTimeSerializer

--- modules/app-server/rest/src/main/kotlin/rest/Serializers.kt
+++ modules/app-server/rest/src/main/kotlin/rest/Serializers.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import java.time.OffsetDateTime
 import java.util.*
 
 object UUIDSerializer : KSerializer<UUID> {
@@ -15,6 +16,18 @@ object UUIDSerializer : KSerializer<UUID> {
     }
 
     override fun serialize(encoder: Encoder, value: UUID) {
+        encoder.encodeString(value.toString())
+    }
+}
+
+object OffsetDateTimeSerializer : KSerializer<OffsetDateTime> {
+    override val descriptor = PrimitiveSerialDescriptor("OffsetDateTime", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): OffsetDateTime {
+        return OffsetDateTime.parse(decoder.decodeString())
+    }
+
+    override fun serialize(encoder: Encoder, value: OffsetDateTime) {
         encoder.encodeString(value.toString())
     }
 }
