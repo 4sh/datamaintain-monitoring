66fbb8b785f03fa873fb97160e545d0925600bf0
asolovieff <alexandre.solovieff@4sh.fr>
2024-01-23 14:08:47 +0100

    feat: Add /scriptExecutions route

--- modules/app-server/rest/src/main/kotlin/rest/v1/route/script/execution/ScriptExecutionRoutesV1.kt
+++ modules/app-server/rest/src/main/kotlin/rest/v1/route/script/execution/ScriptExecutionRoutesV1.kt
@@ -1,9 +1,11 @@
 package rest.v1.route.script.execution
 
+import execution.Status
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import rest.v1.route.script.execution.dto.toDtoV1
+import script.execution.ScriptExecutionSearchRequest
 import script.execution.ScriptExecutionService
 import java.util.UUID
 
@@ -12,7 +14,19 @@ internal const val scriptExecutionId = "scriptExecutionId"
 internal fun ApplicationCall.scriptExecutionId() = UUID.fromString(this.parameters[scriptExecutionId])
 
 internal fun Route.scriptExecutionV1Routes(scriptExecutionService: ScriptExecutionService) {
-    get("/scriptExecutions/{$scriptExecutionId}") {
-        call.respondNullable(call.scriptExecutionId()?.let { scriptExecutionService.findOneDetailById(it)?.toDtoV1() })
+    route("/scriptExecutions") {
+        get {
+            val scriptExecutionSearchRequest =
+                ScriptExecutionSearchRequest(
+                    status = call.parameters["status"]?.let { Status.valueOf(it) },
+                    batchExecutionRef = call.parameters["batchExecutionRef"]?.let { UUID.fromString(it) }
+                )
+
+            call.respond(scriptExecutionService.find(scriptExecutionSearchRequest).map { it.toDtoV1() })
+        }
+
+        get("/{$scriptExecutionId}") {
+            call.respondNullable(call.scriptExecutionId()?.let { scriptExecutionService.findOneDetailById(it)?.toDtoV1() })
+        }
     }
 }
