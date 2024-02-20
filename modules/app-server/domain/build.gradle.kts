b82179f367bd67b4ff8f163cc1466a5dfa3b6b5d
Élise Rubio <elise.rubio@4sh.fr>
2024-02-20 18:29:31 +0100

    :heavy_plus_sign: Add test dependencies in domain build.gradle.kts

--- modules/app-server/domain/build.gradle.kts
+++ modules/app-server/domain/build.gradle.kts
@@ -9,7 +9,11 @@ repositories {
 }
 
 dependencies {
-
+    testImplementation("io.strikt:strikt-core:0.34.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testImplementation("io.mockk:mockk:1.12.4")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.7.0")
 }
 
 val compileKotlin: KotlinCompile by tasks
@@ -19,4 +23,10 @@ compileKotlin.kotlinOptions {
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
     jvmTarget = Versions.java
+}
+
+tasks.getByPath("test").doFirst {
+    with(this as Test) {
+        useJUnitPlatform()
+    }
 }
