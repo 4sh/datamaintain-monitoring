c20f226195c3733b7797fbd74ee38ff775ce22a6
Élise Rubio <elise.rubio@4sh.fr>
2024-03-19 11:44:31 +0100

    :coffin: Remove useless imports

--- modules/app-server/server/src/main/kotlin/MappingUtils.kt
+++ modules/app-server/server/src/main/kotlin/MappingUtils.kt
@@ -1,8 +1,5 @@
 import com.google.protobuf.Timestamp
 import java.time.Instant
-import java.time.LocalDateTime
-import java.time.OffsetDateTime
-import java.time.ZoneOffset
 
 fun Timestamp.toInstant(): Instant =
     Instant.ofEpochSecond(seconds, nanos.toLong())
