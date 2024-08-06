6e0a387c46d0f28df31cedc14b9080089da9ada9
Élise Rubio <elise.rubio@4sh.fr>
2024-08-06 18:32:13 +0200

    :sparkles: Load script from backend

--- modules/ui/src/routes/scripts/[id]/+page.svelte
+++ modules/ui/src/routes/scripts/[id]/+page.svelte
@@ -1,1 +1,20 @@
-Scripts [id]
+<script lang="ts">
+    import type {Script} from '$lib/domain/script/Script';
+    import {page} from '$app/stores';
+    import {ScriptService} from '$lib/services/ScriptService';
+
+    let scriptPromise: Promise<Script>
+    $: if($page.params?.id) {
+        scriptPromise = ScriptService.byId($page.params.id)
+    }
+
+</script>
+
+Script [id]
+
+{#await scriptPromise}
+    <p>...waiting</p>
+{:then script}
+
+    {script.name}
+{/await}
