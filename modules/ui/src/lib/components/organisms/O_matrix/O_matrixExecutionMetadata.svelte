fb6954b5cd63cbaaaf7724bbbd0386c0e899fcf3
Élise Rubio <elise.rubio@4sh.fr>
2024-10-01 17:08:39 +0200

    :lipstick: Use table component to display matrix module env

--- modules/ui/src/lib/components/organisms/O_matrix/O_matrixExecutionMetadata.svelte
+++ modules/ui/src/lib/components/organisms/O_matrix/O_matrixExecutionMetadata.svelte
@@ -6,6 +6,8 @@
 
 {#if metadata}
     {metadata.status}
-    <div><a href="/executions/{metadata.executionId}">{metadata.executionDate.toLocaleDateString()}</a></div>
+    {#if metadata.executionDate}
+        <div><a href="/executions/{metadata.executionId}">{metadata.executionDate.toLocaleDateString()}</a></div>
+    {/if}
     <div>{metadata.duration}</div>
 {/if}
--- modules/ui/src/lib/components/organisms/O_matrix/O_matrixModuleEnv.svelte
+++ modules/ui/src/lib/components/organisms/O_matrix/O_matrixModuleEnv.svelte
@@ -1,34 +1,37 @@
 <script lang="ts">
-    import {ModuleEnvMatrix} from "$lib/domain/ModuleEnvMatrix";
+    import type {ModuleEnvMatrix} from "$lib/domain/ModuleEnvMatrix";
     import O_matrixExecutionMetadata from "$lib/components/organisms/O_matrix/O_matrixExecutionMetadata.svelte";
+    import M_table from '$lib/components/molecules/table/M_table.svelte';
+    import M_tableHeaderItem from '$lib/components/molecules/table/M_tableHeaderItem.svelte';
+    import M_tableBodyItem from '$lib/components/molecules/table/M_tableBodyItem.svelte';
 
     export let projectRef: string
     export let matrix: ModuleEnvMatrix
 </script>
 
-<table class="table-auto">
-    <thead>
-    <tr>
-        <th></th>
+<M_table>
+    <svelte:fragment slot="headerContent">
+        <M_tableHeaderItem>
+            Nom du module
+        </M_tableHeaderItem>
         {#each matrix.envs as { id, name }}
-            <th><a href="/projects/{projectRef}/envs/{id}">{name}</a></th>
+            <M_tableHeaderItem><a href="/projects/{projectRef}/envs/{id}">{name}</a></M_tableHeaderItem>
         {/each}
-    </tr>
-    </thead>
-    <tbody>
+    </svelte:fragment>
+    <svelte:fragment slot="bodyContent">
+        {#each matrix.entries as entry}
 
-    {#each matrix.entries as entry}
-        <tr>
-            <td>
+            <tr>
+                <M_tableBodyItem>
                 <a href="/projects/{projectRef}/modules/{entry.moduleId}">{entry.moduleName}</a>
-            </td>
+                </M_tableBodyItem>
 
             {#each matrix.envs as { id, name }}
-                <td class="px-2">
+                <M_tableBodyItem>
                     <O_matrixExecutionMetadata metadata="{entry.getEnvExecutionEntryMetadata(id)?.execution}"/>
-                </td>
+                </M_tableBodyItem>
             {/each}
         </tr>
-    {/each}
-    </tbody>
-</table>
+        {/each}
+    </svelte:fragment>
+</M_table>
