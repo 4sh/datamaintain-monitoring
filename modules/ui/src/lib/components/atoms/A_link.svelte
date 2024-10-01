55f037a3caf4491cbb3da5bcd3533a70756d16ec
Élise Rubio <elise.rubio@4sh.fr>
2024-10-01 17:13:28 +0200

    :lipstick: Add link atom & use it

---
+++ modules/ui/src/lib/components/atoms/A_link.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export let href: string;
+</script>
+
+<a class="link" href="{href}">
+    <slot></slot>
+</a>
+
+<style lang="scss">
+  @import "src/app";
+
+  .link {
+    color: $app-primary_700;
+
+    &:hover {
+      cursor: pointer;
+      text-decoration: underline;
+    }
+  }
+
+</style>
--- modules/ui/src/lib/components/organisms/O_matrix/O_matrixModuleEnv.svelte
+++ modules/ui/src/lib/components/organisms/O_matrix/O_matrixModuleEnv.svelte
@@ -4,6 +4,7 @@
     import M_table from '$lib/components/molecules/table/M_table.svelte';
     import M_tableHeaderItem from '$lib/components/molecules/table/M_tableHeaderItem.svelte';
     import M_tableBodyItem from '$lib/components/molecules/table/M_tableBodyItem.svelte';
+    import A_link from '$lib/components/atoms/A_link.svelte';
 
     export let projectRef: string
     export let matrix: ModuleEnvMatrix
@@ -15,7 +16,9 @@
             Nom du module
         </M_tableHeaderItem>
         {#each matrix.envs as { id, name }}
-            <M_tableHeaderItem><a href="/projects/{projectRef}/envs/{id}">{name}</a></M_tableHeaderItem>
+            <M_tableHeaderItem>
+                <A_link href="/projects/{projectRef}/envs/{id}">{name}</A_link>
+            </M_tableHeaderItem>
         {/each}
     </svelte:fragment>
     <svelte:fragment slot="bodyContent">
@@ -23,10 +26,12 @@
 
             <tr>
                 <M_tableBodyItem>
-                <a href="/projects/{projectRef}/modules/{entry.moduleId}">{entry.moduleName}</a>
+                    <A_link href="/projects/{projectRef}/modules/{entry.moduleId}">
+                        {entry.moduleName}
+                    </A_link>
                 </M_tableBodyItem>
 
-            {#each matrix.envs as { id, name }}
+            {#each matrix.envs as { id }}
                 <M_tableBodyItem>
                     <O_matrixExecutionMetadata metadata="{entry.getEnvExecutionEntryMetadata(id)?.execution}"/>
                 </M_tableBodyItem>
